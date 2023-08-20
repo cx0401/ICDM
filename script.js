f064cfe75e382f0954f8a1ea91c5ccdcfdb677ec
cx0401 <1403690686@qq.com>
2023-08-20 23:09:54 +0800

    add the new

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
         // For example:
         const extractedSkills = extractSkills(input);
         outputText.value = extractedSkills.join(", ");
-        logArea.innerHTML = "Extraction completed.";
+        logArea.innerHTML = " 第一步,总结该求职者的项目经历和工作经历:该求职者在北京安盟信息技术股份有限公司担任Linux C/C++开发工程师一职,工作职位为C,工作描述包括在网闸实现传输和解析、利用iptables解析协议、使用C调用wreshark动态库解析协议等。之后在北京安信华科技有限公司担任服务器挂马识别、短信和邮件告警等相关工作。同时,该求职者在北京蓝色星际科技股份有限公司和北京左江科技股份有限公司也从事了Linux C研发的工作。第二步,分析该求职者的经验如何匹配招聘需求:根据招聘需求,该职位要求候选人精通C语言程序设计,有3年以上Linux下C语言开发经验,熟悉Linux环境和Linux下各种IPC通信方式,并且熟悉TCP/IP协议以及编程和常见网络协议。从求职者的工作经历来看,他在北京安盟信息技术股份有限公司和北京安信华科技有限公司都有从事Linux C/C++开发的工作,并且有3年以上的Linux下C语言开发经验。他还在项目中使用了GDB等调试工具,并熟练使用Linux下各种管理工具。因此,他的经验与招聘需求相匹配。第三步,从教育经历等其他信息分析求职者的个人综合实力:该求职者拥有本科学历,专业为通信工程,在南阳师范学院就读。他工作年限为9年,有丰富的工作经验。从他的工作经历来看,他从事了与Linux C开发相关的工作,并且有开发大型项目的经验。此外,他还参与了TCP/IP协议的编写,具备数据库开发经验和Linux内核裁剪经验,这些都能证明他的技术能力和综合能力。最后的总结:根据该求职者的项目经历、工作经历和教育经历来看,他具备了招聘需求中要求的精通C语言和3年以上Linux下C语言开发经验等技能。他在通信工程专业和有9年工作经验的基础上,展现了在Linux C研发方面的专业能力和项目经验。因此,他是一个很好的匹配的候选人,值得考虑录取。";
     });
 
     function extractSkills(text) {
